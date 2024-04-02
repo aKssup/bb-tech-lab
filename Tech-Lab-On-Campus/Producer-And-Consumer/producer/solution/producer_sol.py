ee7bcb0317de26d9ba598fece79ac6e13b6d27f9
aKssup <118090347+aKssup@users.noreply.github.com>
2024-04-02 18:26:30 -0400

    producer update

--- Tech-Lab-On-Campus/Producer-And-Consumer/producer/solution/producer_sol.py
+++ Tech-Lab-On-Campus/Producer-And-Consumer/producer/solution/producer_sol.py
@@ -15,20 +15,20 @@ class mqProducer(mqProducerInterface):
     def setupRMQConnection(self):
         # Set-up Connection to RabbitMQ service
         self.con_params = pika.URLParameters(os.environ["AMQP_URL"])
-        self.connection = pika.BlockingConnection(parameters=con_params)
+        self.connection = pika.BlockingConnection(parameters=self.con_params)
 
         # Establish Channel
         self.channel = self.connection.channel()
 
         # Create the exchange if not already present
-        self.exchange = self.channel.exchange_declare(exchange="Exchange Name")
+        self.exchange = self.channel.exchange_declare(exchange= self.exchange_name)
         pass
 
     def publishOrder(self, message: str):
         # Basic Publish to Exchange
         self.channel.basic_publish(
-        exchange="Exchange Name",
-        routing_key="Routing Key",
+        exchange= self.exchange_name,
+        routing_key= self.routing_key,
         body=message,
         )
 
